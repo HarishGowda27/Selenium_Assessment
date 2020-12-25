e7e270d90adfb14f55aedb619ae4aca6009d0bfb
HarishGowda27 <43615027+HarishGowda27@users.noreply.github.com>
2020-12-26 00:19:16 +0530

    Update Git_Test.java

--- src/test/java/com/testscript/Git_Test.java
+++ src/test/java/com/testscript/Git_Test.java
@@ -1,7 +1,7 @@
 package com.testscript;
 
 import java.util.concurrent.TimeUnit;
-
+//BY HarishGowda
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
